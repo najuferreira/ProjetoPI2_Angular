f6322cd231b060057f61588ceed027f4521ec4e3
Sthephany Viana da Silva <sthephanyv021@gmail.com>
2025-05-19 11:07:45 -0300

    add new function

--- projeto-pi-angular/src/app/components/pages/admin/header-admin/header-admin.component.ts
+++ projeto-pi-angular/src/app/components/pages/admin/header-admin/header-admin.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Rou, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-admin',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header-admin.component.html',
   styleUrls: ['./header-admin.component.scss']
 })
