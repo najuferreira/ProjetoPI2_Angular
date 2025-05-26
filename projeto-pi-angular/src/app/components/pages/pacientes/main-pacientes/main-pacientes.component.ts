72ab3feeacf95b384851d87c13ef41670ea86405
Sthephany Viana da Silva <sthephanyv021@gmail.com>
2025-05-26 13:05:06 -0300

    add new  funtion

--- projeto-pi-angular/src/app/components/pages/pacientes/main-pacientes/main-pacientes.component.ts
+++ projeto-pi-angular/src/app/components/pages/pacientes/main-pacientes/main-pacientes.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Paciente } from '../../../../../interfaces/user';
 import { UserService } from '../../../../../services/user.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-pacientes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './main-pacientes.component.html',
   styleUrls: ['./main-pacientes.component.scss']
 })
@@ -14,11 +15,60 @@ export class MainPacientesComponent implements OnInit {
   pacienteData: Paciente[] = [];
   isLoading: boolean = true;
   error: string | null = null;
+  pacienteForm!: FormGroup;
 
-  constructor(private userService: UserService) { }
+
+  constructor(private userService: UserService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.loadPatients();
+
+    this.pacienteForm = this.fb.group({
+      nome: [''],
+      data_nascimento: [''],
+      cpf: [''],
+      rg: [''],
+      numero_contato: [''],
+      email: [''],
+      genero: [''],
+      preferencia_pagamento: [''],
+      profissao: [''],
+      cep: [''],
+      motivo_contato: [''],
+      horario_atendimento: [''],
+      conheceu_servico: [''],
+      contato_emergencia: [''],
+    });
+  };
+
+  registrarPaciente() {
+    const novoUser: Omit<Paciente, 'id' | 'isAdmin' | 'password'> = {
+      nome: this.pacienteForm.value.nome,
+      data_nascimento: this.pacienteForm.value.data_nascimento,
+      cpf: this.pacienteForm.value.cpf,
+      rg: this.pacienteForm.value.rg,
+      numero_contato: this.pacienteForm.value.numero_contato,
+      email: this.pacienteForm.value.email,
+      genero: this.pacienteForm.value.genero,
+      preferencia_pagamento: this.pacienteForm.value.preferencia_pagamento,
+      profissao: this.pacienteForm.value.profissao,
+      cep: this.pacienteForm.value.cep,
+      motivo_contato: this.pacienteForm.value.motivo_contato,
+      horario_atendimento: this.pacienteForm.value.horario_atendimento,
+      conheceu_servico: this.pacienteForm.value.conheceu_servico,
+      contato_emergencia: this.pacienteForm.value.contato_emergencia
+    }
+    console.log(`Novo usuário: ${novoUser}`);
+
+
+    this.userService.postPaciente(novoUser).subscribe({
+      next: (response) => {
+        console.log(`Paciente criado ${response}`);
+      },
+      error(e) {
+        console.error(`Erro ao criar Estoque ${e}`);
+      }
+    })
   }
 
   loadPatients(): void {
--- projeto-pi-angular/src/services/user.service.ts
+++ projeto-pi-angular/src/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Paciente } from '../interfaces/user';
-import { Observable } from 'rxjs';
+import { Paciente, User } from '../interfaces/user';
+import { Observable, retry } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -10,16 +10,22 @@ import { HttpClient } from '@angular/common/http';
 export class UserService {
 
   constructor(private http: HttpClient) { }
-  
+
   urlUser = "http://localhost:3000/user";
 
   getAllPaciente() {
     let data: Observable<Paciente[]>;
     data = this.http.get<Paciente[]>(this.urlUser);
     return data;
-  }
+  };
 
-  postPaciente() {
-    
-  }
-}
+  postPaciente(user: Omit<Paciente, 'id' | 'isAdmin' | 'password'>) {
+    const userCompleto = {
+      ...user,
+      isAdmin: false
+    };
+    console.log("User Completo", userCompleto);
+
+    return this.http.post<Paciente>(this.urlUser, userCompleto);
+  };
+};
